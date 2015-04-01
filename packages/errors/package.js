fb6eafe36e147a6123dc5be1ad9acc8c6c7d54d4
Oladayo Osikoya <alwaysdeone@gmail.com>
2015-04-01 14:15:57 +0100

    Created basic errors package and linked it in.

---
+++ packages/errors/package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'deone:errors',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.5');
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+  if (api.export)
+    api.export('Errors');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('deone:errors');
+  api.addFiles('errors-tests.js');
+});
