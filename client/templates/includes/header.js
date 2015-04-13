ef95a34f4711e0851f7c1f63c112370b799a35ab
Oladayo Osikoya <alwaysdeone@gmail.com>
2015-04-13 12:49:28 +0100

    Added post reordering animation.

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -1,12 +1,10 @@
 Template.header.helpers({
 	activeRouteClass: function(/* route names */) {
-		console.log(arguments);
 		var args = Array.prototype.slice.call(arguments, 0);
 		// console.log(args);
 		args.pop();
 
 		var active = _.any(args, function(name) {
-			console.log(Router.current());
 			return Router.current() && Router.current().route.getName() === name
 		});
 
